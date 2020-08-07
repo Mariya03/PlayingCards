74169ad6a3bd3b23dbba323a75e1d0e8cd2426d9
Андрейс Жестянниковс <azhestyannikovs@edu.hse.ru>
2020-08-07 15:53:40 +0300

    Добавьте файлы проекта.

---
+++ Desck.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace PlayingCards
+{
+    partial class Desck
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Desck));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.LoadCards = new System.Windows.Forms.ToolStripButton();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.LoadCards});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(531, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // LoadCards
+            // 
+            this.LoadCards.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.LoadCards.Image = ((System.Drawing.Image)(resources.GetObject("LoadCards.Image")));
+            this.LoadCards.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.LoadCards.Name = "LoadCards";
+            this.LoadCards.Size = new System.Drawing.Size(70, 22);
+            this.LoadCards.Text = "Load Cards";
+            this.LoadCards.Click += new System.EventHandler(this.LoadCards_Click);
+            // 
+            // Desck
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(531, 450);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Desck";
+            this.Text = "Form1";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton LoadCards;
+    }
+}
+
---
+++ Desck.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace PlayingCards
+{
+    public partial class Desck : Form
+    {
+        private string folderPath = null;
+        private string[] fileNames = null ;
+
+        public Desck()
+        {
+            InitializeComponent();
+            InitializeDesck();
+        }
+
+        private void InitializeDesck()
+        {
+            this.BackColor = Color.Green;
+        }
+
+        private void LoadCards_Click(object sender, EventArgs e)
+        {
+            folderPath = @"C:\Users\Андрей\Downloads\Playing Cards\playing_card_images\face";
+            fileNames = Directory.GetFiles(folderPath);
+        }
+    }
+}
