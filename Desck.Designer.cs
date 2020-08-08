14288964bf851c0135783a9fb7f5066835478d4f
Андрейс Жестянниковс <azhestyannikovs@edu.hse.ru>
2020-08-08 17:04:03 +0300

    version2

--- Desck.Designer.cs
+++ Desck.Designer.cs
@@ -31,16 +31,21 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Desck));
             this.toolStrip1 = new System.Windows.Forms.ToolStrip();
             this.LoadCards = new System.Windows.Forms.ToolStripButton();
+            this.StackCards = new System.Windows.Forms.ToolStripButton();
+            this.DeckCards = new System.Windows.Forms.ToolStripButton();
             this.toolStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // toolStrip1
             // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.LoadCards});
+            this.LoadCards,
+            this.StackCards,
+            this.DeckCards});
             this.toolStrip1.Location = new System.Drawing.Point(0, 0);
             this.toolStrip1.Name = "toolStrip1";
-            this.toolStrip1.Size = new System.Drawing.Size(531, 25);
+            this.toolStrip1.Size = new System.Drawing.Size(708, 31);
             this.toolStrip1.TabIndex = 0;
             this.toolStrip1.Text = "toolStrip1";
             // 
@@ -50,18 +55,39 @@
             this.LoadCards.Image = ((System.Drawing.Image)(resources.GetObject("LoadCards.Image")));
             this.LoadCards.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.LoadCards.Name = "LoadCards";
-            this.LoadCards.Size = new System.Drawing.Size(70, 22);
+            this.LoadCards.Size = new System.Drawing.Size(87, 28);
             this.LoadCards.Text = "Load Cards";
             this.LoadCards.Click += new System.EventHandler(this.LoadCards_Click);
             // 
+            // StackCards
+            // 
+            this.StackCards.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.StackCards.Image = ((System.Drawing.Image)(resources.GetObject("StackCards.Image")));
+            this.StackCards.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.StackCards.Name = "StackCards";
+            this.StackCards.Size = new System.Drawing.Size(48, 28);
+            this.StackCards.Text = "Stack";
+            this.StackCards.Click += new System.EventHandler(this.StackCards_Click);
+            // 
+            // DeckCards
+            // 
+            this.DeckCards.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.DeckCards.Image = ((System.Drawing.Image)(resources.GetObject("DeckCards.Image")));
+            this.DeckCards.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.DeckCards.Name = "DeckCards";
+            this.DeckCards.Size = new System.Drawing.Size(46, 28);
+            this.DeckCards.Text = "Deck";
+            this.DeckCards.Click += new System.EventHandler(this.DeckCards_Click);
+            // 
             // Desck
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(531, 450);
+            this.ClientSize = new System.Drawing.Size(708, 554);
             this.Controls.Add(this.toolStrip1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Desck";
-            this.Text = "Form1";
+            this.Text = "Desck";
             this.toolStrip1.ResumeLayout(false);
             this.toolStrip1.PerformLayout();
             this.ResumeLayout(false);
@@ -73,6 +99,8 @@
 
         private System.Windows.Forms.ToolStrip toolStrip1;
         private System.Windows.Forms.ToolStripButton LoadCards;
+        private System.Windows.Forms.ToolStripButton StackCards;
+        private System.Windows.Forms.ToolStripButton DeckCards;
     }
 }
 
--- Desck.cs
+++ Desck.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
+using System.Net.Mime;
+using System.Drawing.Text;
 
 namespace PlayingCards
 {
@@ -16,6 +18,8 @@ namespace PlayingCards
         private string folderPath = null;
         private string[] fileNames = null ;
         private Random rand = new Random();
+        private List<PictureBox> cards = new List<PictureBox>();
+
 
         public Desck()
         {
@@ -28,26 +32,85 @@ namespace PlayingCards
             this.BackColor = Color.Green;
         }
 
+        private string SelectFolder()
+        {
+            var selectFolderDialog = new FolderBrowserDialog();
+            DialogResult result = selectFolderDialog.ShowDialog();
+
+            if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(selectFolderDialog.SelectedPath))
+            {
+                return selectFolderDialog.SelectedPath;
+            }
+            return null;
+        }
         private void LoadCards_Click(object sender, EventArgs e)
         {
            PictureBox filePictureBox = null;
-           folderPath = @"C:\Users\Андрей\Downloads\Playing Cards\playing_card_images\face";
-            fileNames = Directory.GetFiles(folderPath);
+           folderPath = @"C:\Users\Lietotajs\Downloads\Playing Cards\Playing Cards\playing_card_images\face";
             
-
-            foreach(var fileName in fileNames)
+          // folderPath = SelectFolder();
+            if(folderPath == null)
             {
+                return;
+            }
+            fileNames = Directory.GetFiles(folderPath);
+
+            foreach (var fileName in fileNames)
+            {
+               
+
                 filePictureBox = new PictureBox()
                 {
                     Height = 100,
                     Width = 70,
                     SizeMode = PictureBoxSizeMode.StretchImage,
-                    Left = rand.Next(0, 400),
-                    Top = rand.Next(50, 300),
+                    Left = rand.Next(0, 800),
+                    Top = rand.Next(50, 600),
                     Image = Image.FromFile( fileName)
                 };
+                filePictureBox.Click += Card_Click;
                 this.Controls.Add(filePictureBox);
+                cards.Add(filePictureBox);
             }
         }
+
+        private void StackCards_Click(object sender, EventArgs e)
+        {
+            int x = 100, y = 100;
+
+            foreach (var card in cards)
+            {
+                card.Location = new Point(x, y);
+                x++;
+                y++;
+            }
+        }
+
+        private void DeckCards_Click(object sender, EventArgs e)
+        {
+            int counter = 0;
+            for(int x = 1;  x < 10; x++)
+            {
+                for(int y = 1; y <7; y++)
+                {
+                    cards[counter].Location = new Point(x * 75, y * 105);
+                    counter++;
+                }
+            }
+
+            ////private void Card_Click(object sender, EventArgs e)
+           // {
+               //// var card = (PictureBox)sender;
+               // card.Location = new Point(0, 0);
+           // }
+        }
+
+        private void Card_Click(object sender, EventArgs e)
+        {
+            var card = (PictureBox)sender;
+            card.Location = new Point(30, 30);
+            card.BringToFront();
+        }
     }
+
 }
